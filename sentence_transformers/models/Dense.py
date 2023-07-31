80d76e1356e57e39251cc8936fcbe39a62299db5
Markus Ekvall <paradoxen93@gmail.com>
2023-07-31 19:47:28 +0200

    fixed muenihoffs Dense

--- sentence_transformers/models/Dense.py
+++ sentence_transformers/models/Dense.py
@@ -11,7 +11,7 @@ from ..util import fullname, import_from_string
 class Dense(nn.Module):
     """Feed-forward function with  activiation function.
 
-    This layer takes a fixed-sized sentence embedding and passes it through a feed-forward layer. Can be used to generate deep averaging networks (DAN).
+    This layer takes a fixed-sized sentence embedding and passes it through a feed-forward layer. Can be used to generate deep averaging networs (DAN).
 
     :param in_features: Size of the input dimension
     :param out_features: Output size
@@ -20,13 +20,16 @@ class Dense(nn.Module):
     :param init_weight: Initial value for the matrix of the linear layer
     :param init_bias: Initial value for the bias of the linear layer
     """
-    def __init__(self, in_features: int, out_features: int, bias: bool = True, activation_function=nn.Tanh(), init_weight: Tensor = None, init_bias: Tensor = None):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, activation_function=nn.Tanh(), init_weight: Tensor = None, init_bias: Tensor = None, key_name: str = "sentence_embedding"):
         super(Dense, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.bias = bias
         self.activation_function = activation_function
         self.linear = nn.Linear(in_features, out_features, bias=bias)
+
+        # key_name added by Muennighoff
+        self.key_name = key_name
 
         if init_weight is not None:
             self.linear.weight = nn.Parameter(init_weight)
@@ -35,14 +38,16 @@ class Dense(nn.Module):
             self.linear.bias = nn.Parameter(init_bias)
 
     def forward(self, features: Dict[str, Tensor]):
-        features.update({'sentence_embedding': self.activation_function(self.linear(features['sentence_embedding']))})
+        # key_name added by Muennighoff
+        features.update({self.key_name: self.activation_function(self.linear(features[self.key_name]))})
         return features
 
     def get_sentence_embedding_dimension(self) -> int:
         return self.out_features
 
     def get_config_dict(self):
-        return {'in_features': self.in_features, 'out_features': self.out_features, 'bias': self.bias, 'activation_function': fullname(self.activation_function)}
+        # key_name added by Muennighoff
+        return {'in_features': self.in_features, 'out_features': self.out_features, 'bias': self.bias, 'activation_function': fullname(self.activation_function), 'key_name': self.key_name}
 
     def save(self, output_path):
         with open(os.path.join(output_path, 'config.json'), 'w') as fOut:
@@ -52,7 +57,6 @@ class Dense(nn.Module):
 
     def __repr__(self):
         return "Dense({})".format(self.get_config_dict())
-
     @staticmethod
     def load(input_path):
         with open(os.path.join(input_path, 'config.json')) as fIn:
